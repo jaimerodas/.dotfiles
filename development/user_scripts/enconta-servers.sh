5783fd8042614ff623bf26f31a310230aa5c1ff9
Jaime Rodas <jaime@resuelve.mx>
2018-05-14 13:34:04 -0500

    Quita cosas innecesarias de enconta-servers

--- development/user_scripts/enconta-servers.sh
+++ development/user_scripts/enconta-servers.sh
@@ -4,8 +4,6 @@ set -e
 
 environment=${1:-jobs}
 environment_name=encontaapp-$environment
-username=ec2-user
-pem_file=~/.ssh/$environment_name.pem
 
 function list {
 	aws elasticbeanstalk describe-environment-resources --environment-name "$environment_name" |
