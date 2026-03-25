8756413766d68e5ceb53ad235f149ea2c20e48cc
Jaime Rodas <jaime@rodas.mx>
2026-03-25 16:24:12 -0600

    config.fish tiene mise de homebrew
    Estaba puesto como si instaláramos mise directamente en lugar de a través de homebrew.

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # editor
 set -gx VISUAL nova
 
-~/.local/bin/mise activate fish | source
+/opt/homebrew/bin/mise activate fish | source
 
 # Added by OrbStack: command-line tools and integration
 source ~/.orbstack/shell/init2.fish 2>/dev/null; or :
